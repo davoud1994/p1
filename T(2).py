755a8a5010c3b147cff75ea4a0f30f48740fce45
davoud1994 <davoudgh1373@gmail.com>
2023-06-10 18:00:39 +0330

    add_2

---
+++ T(2).py
@@ -0,0 +1,37 @@
+# S = input("write =")
+# how_many = input("chr =")
+# print(S.count(how_many))
+# یک کاراکتر چندبار تکرار شده است
+# ........................>
+# s = input("write =")
+# for i in (0, len(s) - 1):
+#     if i == len(s) - 1:
+#         print(s[len(s)-1])
+# یک جمله از کاربر بگیریم حرف اخرش رو چاپ کنیم
+#
+# write = input("write =")
+# Len = len(write) - 1
+# print(write[Len])
+# در یه جمله اخرین حرف رو پیدا کن
+#
+# write = input("write =")
+# end = input("end =")
+# number = write.rfind(" ")
+# print(write[number + 1:].rstrip(end))
+# (2)یک جمله از کاربر بگیریم و اخرین حرفش رو چاپ کنیم
+# --------------------------------->
+# write1 = input("write1 =")
+# write2 = input("write2 =")
+# print(write2 in write1)
+# دو جمله داریم ایا جمله دوم درون جمله یکم است
+# -------------------->
+# write = input("str:")
+# print(write.replace(" ", ""))
+# for s in (0, len(write) - 1):
+#     if write[s] == "":
+#         print(write[s])
+#     else:
+#         print(write[s], end=" ")
+# جابه جایی دو کاراکتر باهم و گرفتن
+# رشته و فقط عناصر اول واخر را چاپ کردن
+# ---------------------------------->
---
+++ p(2).py
@@ -0,0 +1,23 @@
+# write = input("enter your string:")
+# sentence = write.count(".")
+# word = write.count(" ") + 1
+# character = write.count(".") + \
+#             write.count(",") + \
+#             write.count("?") + \
+#             write.count(" ")
+# letter = len(write) - character
+# print("Number of sentences =", sentence)
+# print("Number of word =", word)
+# print("Number of character =", character)
+# print("Number of letter =", letter)
+# دادن یک متن و بررسی اجزا موجود در ان(1)
+# --------------------------------->
+# write = input("enter your character:")
+# print("unicode is :", ord(write))
+# چاپ یوینکد(2)
+# ------------------------------->
+# number = input("enter your phone number:")
+# true = number.isnumeric()
+# print("is your phone number a number?", true)
+# phone number(3)
+# -------------------------------------->
